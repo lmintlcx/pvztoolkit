b8190dfea117226bee9af6df777c6da7d8bd5c7e
lmintlcx <lmintlcx@gmail.com>
2020-06-07 06:12:34 +0800

    update 1.2.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,44 @@
 
 #include <cassert>
 
-#include "window.h"
+#include "window.zh.h"
+#include "window.en.h"
 
 #include <Windows.h>
 #include <ShellAPI.h>
 
+#define IDI_ICON 1001
+
 #if _MSC_VER >= 1910
 static_assert(sizeof(void *) == 4);
 #endif
+
+void choice_callback(Fl_Widget *w, void *)
+{
+  HKEY hKey;
+  DWORD ret;
+  ret = RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Cube Studio\\PvZ Toolkit\\v1", //
+                      0, KEY_SET_VALUE, &hKey);
+  if (ret != ERROR_SUCCESS)
+    ret = RegCreateKeyExW(HKEY_CURRENT_USER, L"Software\\Cube Studio\\PvZ Toolkit\\v1", //
+                          0, nullptr, 0, KEY_SET_VALUE, nullptr, &hKey, nullptr);
+  if (ret == ERROR_SUCCESS)
+  {
+    TCHAR szLangChinese[] = L"简体中文";
+    TCHAR szLangEnglish[] = L"English";
+    if (((Pt::zh::Fl_Choice_ *)w)->value() == 1)
+      ret = RegSetValueExW(hKey, L"Language", 0, REG_SZ, (LPBYTE)&szLangEnglish, sizeof(szLangEnglish));
+    else
+      ret = RegSetValueExW(hKey, L"Language", 0, REG_SZ, (LPBYTE)&szLangChinese, sizeof(szLangChinese));
+    std::cout << ((Pt::zh::Fl_Choice_ *)w)->value() << " " << ret << std::endl;
+    RegCloseKey(hKey);
+  }
+}
+
+void button_callback(Fl_Widget *w, void *data)
+{
+  ((Fl_Double_Window *)data)->hide();
+}
 
 void window_callback(Fl_Widget *, void *)
 {
@@ -17,12 +47,14 @@ void window_callback(Fl_Widget *, void *)
   exit(0);
 }
 
+/// main ///
+
 int main(int argc, char **argv)
 {
   // 初始化随机数种子
   std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-  // 输出中文
+  // 调试输出中文
 #ifdef _DEBUG
   std::wcout.imbue(std::locale("chs"));
 #endif
@@ -42,11 +74,67 @@ int main(int argc, char **argv)
   if (GetLastError() == ERROR_ALREADY_EXISTS)
     return -1;
 
-  Pt::Window window(450, 270, "PvZ Toolkit");
-  window.callback(window_callback);
+  // 第一次调用时启用线程锁机制
+  Fl::lock();
 
-  Fl::lock(); // 第一次调用时启用线程锁机制
-  window.show(argc, argv);
+  Fl_Window launcher(280, 115, "选择界面语言");
+  Pt::zh::Fl_Choice_ choice(130, 20, 120, 25, "语言(Language): ");
+  Fl_Button button(130, 75, 120, 25, "启动(Launch)");
+  launcher.icon((char *)LoadIcon(fl_display, MAKEINTRESOURCE(IDI_ICON)));
+  choice.add("简体中文");
+  choice.add("English");
+  choice.value(0);
+  choice.callback(choice_callback);
+  button.callback(button_callback, (void *)&launcher);
+
+  bool first_open;
+  HKEY hKey;
+  DWORD r = RegOpenKeyExW(HKEY_CURRENT_USER, L"Software\\Cube Studio\\PvZ Toolkit\\v1", //
+                          0, KEY_QUERY_VALUE, &hKey);
+  if (r == ERROR_SUCCESS)
+  {
+    first_open = false;
+    DWORD dwType = REG_SZ;
+    TCHAR szLang[260];
+    DWORD dwSize = 260;
+    DWORD r = RegQueryValueExW(hKey, L"Language", 0, &dwType, (LPBYTE)&szLang, &dwSize);
+    if (r == ERROR_SUCCESS && std::wstring(szLang) == L"English")
+      choice.value(1);
+    RegCloseKey(hKey);
+  }
+  else // 没有注册表配置就读系统区域
+  {
+    first_open = true;
+    LANGID lang_id = GetUserDefaultLangID();
+    // std::cout << lang_id << std::endl;
+    if (lang_id != 2052 && lang_id != 1028) // 非中文
+      choice.value(1);
+  }
+
+  if (first_open)
+  {
+    choice.do_callback();
+    launcher.set_modal();
+    launcher.show();
+    while (launcher.shown())
+      Fl::wait();
+  }
+
+  Pt::en::Window window_en(450 + 50 * 4, 270, "PvZ Toolkit");
+  Pt::zh::Window window_zh(450, 270, "植物大战僵尸辅助工具");
+  window_en.callback(window_callback);
+  window_zh.callback(window_callback);
+  std::wcout << choice.value() << std::endl;
+  if (choice.value() == 1)
+  {
+    window_en.show(argc, argv);
+    window_en.pvz->FindPvZ();
+  }
+  else
+  {
+    window_zh.show(argc, argv);
+    window_zh.pvz->FindPvZ();
+  }
 
   int ret = Fl::run();
 
--- src/window.en.cpp
+++ src/window.en.cpp
@@ -1,9 +1,9 @@
 
-#include "window.h"
+#include "window.en.h"
 
 #define IDI_ICON 1001
 
-namespace Pt
+namespace Pt::en
 {
 
 const char *plants[] =
@@ -808,7 +808,7 @@ Window::Window(int width, int height, const char *title)
 
     pvz = new PvZ();
     pvz->callback(cb_find_result, this);
-    pvz->FindPvZ();
+    // pvz->FindPvZ(); // 在 main() 里调用
 
     pak = new PAK();
 
@@ -1972,8 +1972,8 @@ void Window::cb_about()
                         + L"✓ 1.1.0.1056 GOTY 2012.06 (zh)\n"                                          //
                         + L"✓ 1.1.0.1056 GOTY 2012.07 (zh)\n"                                          //
                         + L"\n"                                                                        //
-                        + L"Version: 1.1.1\n"                                                          //
-                        + L"Build Date: 2020/05/02\n"                                                  //
+                        + L"Version: 1.2.0\n"                                                          //
+                        + L"Build Date: 2020/06/07\n"                                                  //
                         + L"Toolchain: MSVC 2017 / FLTK 1.3.5\n"                                       //
                         + L"Copyright: © 2020 lmintlcx\n"                                              //
                         + L"Credit: a418569882 kmtohoem\n";
